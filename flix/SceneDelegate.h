2a63c0312fe521dfdc257be6841ebb844920117d
Darya Kaviani <dkaviani@fb.com>
2020-06-24 09:28:15 -0700

    Initial Commit

---
+++ flix/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  flix
+//
+//  Created by dkaviani on 6/24/20.
+//  Copyright © 2020 dkaviani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
